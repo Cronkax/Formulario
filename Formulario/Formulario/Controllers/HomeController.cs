df627c4c50f5372bdb7e76d99f0ec89a519be135
Henrique Manuel Marante Simões <aluno20469@ipt.pt>
2019-04-20 15:45:26 +0100

    Versão inicial do projeto

---
+++ Formulario/Formulario/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Formulario.Controllers
+{
+    public class HomeController : ApiController
+    {
+        public ActionResult Index()
+        {
+            ViewBag.resposta = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string nome, int idade)
+        {
+            string mensgaem = "Você chama-se " + nome + " e tem " + idade + " anos.";
+            ViewVag.resposta = mensagem;
+            return View();
+        }
+    }
+}
